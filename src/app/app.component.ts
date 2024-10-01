af8353a6075464c18e8051db7ec3e9c48c6d09f5
alizaib110 <alizaibbuttar9@gmail.com>
2024-10-02 01:16:06 +0500

    Reactive Form in Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,34 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'reactiveForm';
+  reactiveForm!: FormGroup;
+  submitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.reactiveForm = this.formBuilder.group({
+      id: [''],
+      name: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required, Validators.minLength(3)],
+    });
+  }
+
+  getControls() {
+    debugger;
+    return this.reactiveForm.controls;
+  }
+
+  onSubmit(): void {
+    debugger;
+    this.submitted = true;
+    if (this.reactiveForm.invalid) {
+      return;
+    }
+  }
 }
